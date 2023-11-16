a162c772d9649409c8e960ed59cbfc7364cc2202
natsuking524 <natsuki5240709@yahoo.co.jp>
2023-11-16 22:58:00 +0900

    Javaプログラミング入門学習終了

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,15 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        double a = 10.0 /0.0;
+        System.out.println(a);
+
+
+
+
+
+    }
+
+}
---
+++ src/UseVariable.java
@@ -0,0 +1,15 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number2;
+        String str2;
+        number2=10xyz0;
+        str1="";
+        System.out.print(number1);
+        System.out.print(str1);
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
