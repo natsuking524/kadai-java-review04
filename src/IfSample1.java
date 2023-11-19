a0a33c8fadbaa3bdabc30e8c532b38d54d1b6677
natsuking524 <natsuki5240709@yahoo.co.jp>
2023-11-19 22:06:36 +0900

    Lesson4の練習問題

---
+++ src/IfSample1.java
@@ -0,0 +1,21 @@
+public class IfSample1 {
+
+    public static void main(String[]args) {
+        int score = 80;
+        if(score>=60){
+        System.out.println("合格です！");
+        }
+    }
+}
+
+
+public class IfSample2{
+    public static void main(String[]args) {
+        int score = 80;
+        if(!(score<60)) {
+            System.out.println("合格です！")
+
+        }
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,22 @@
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        int a = 8;
+        int b = 4;
+        int c = 5;
+
+        if (a == b && b == c && c == a) {
+            System.out.println("この三角形は正三角形です。");
+
+        } else if (a == b || b == c || c == a) {
+            System.out.println("この三角形は二等辺三角形です。");
+
+        } else {
+            System.out.println("この三角形は不当辺三角形です。");
+
+        }
+
+    }
+
+}
